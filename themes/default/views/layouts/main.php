93493dc897e704724b4685142bc266302dc10a45
poctsy <poctsy@foxmail.com>
2013-11-08 19:49:40 +0800

    up

--- themes/default/views/layouts/main.php
+++ themes/default/views/layouts/main.php
@@ -29,9 +29,9 @@
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'首页', 'url'=>array('/site/index')),
+				array('label'=>'登陆', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+				array('label'=>'退出 ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
                 array('label'=>'后台管理', 'url'=>array('/admin'), 'visible'=>Yii::app()->user->checkAccess('GeneralAdmin'))
 
 			),
